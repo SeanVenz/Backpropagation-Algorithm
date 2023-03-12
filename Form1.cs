ff8d20715323a554fefa5ae6b42f797773e1df84
Alys Anthea Carillo <alysanthea.carillo@cit.edu>
2023-03-12 21:26:37 +0800

    Add additional training dataset

--- Form1.cs
+++ Form1.cs
@@ -29,93 +29,122 @@ namespace BackPropagation_Calculation
             //initiate inputs, desired outputs and let the algorithm learn
             for(int i = 0; i < Convert.ToInt32(textBox4.Text); i++)
             {
-                nn.setInputs(0, 72430);
-                nn.setDesiredOutput(0, 25.66);
+                nn.setInputs(0, 41080);
+                nn.setDesiredOutput(0, 26.08);
                 nn.learn();
 
-                nn.setInputs(0, 73190);
-                nn.setDesiredOutput(0, 25.79);
+                nn.setInputs(0, 40650);
+                nn.setDesiredOutput(0, 26.07);
                 nn.learn();
 
-                nn.setInputs(0, 71670);
-                nn.setDesiredOutput(0, 25.85);
-                nn.learn();
-
-                nn.setInputs(0, 72940);
-                nn.setDesiredOutput(0, 25.73);
-                nn.learn();
-
-                nn.setInputs(0, 71670);
-                nn.setDesiredOutput(0, 25.85);
-                nn.learn();
-
-                nn.setInputs(0, 72940);
-                nn.setDesiredOutput(0, 25.73);
-                nn.learn();
-
-                nn.setInputs(0, 74770);
-                nn.setDesiredOutput(0, 25.61);
-                nn.learn();
-
-                nn.setInputs(0, 76320);
-                nn.setDesiredOutput(0, 25.7);
-                nn.learn();
-
-                nn.setInputs(0, 78100);
-                nn.setDesiredOutput(0, 25.8);
-                nn.learn();
-
-                nn.setInputs(0, 70810);
-                nn.setDesiredOutput(0, 25.86);
-                nn.learn();
-
-                nn.setInputs(0, 74560);
-                nn.setDesiredOutput(0, 25.85);
-                nn.learn();
-
-                nn.setInputs(0, 76730);
-                nn.setDesiredOutput(0, 25.52);
-                nn.learn();
-
-                nn.setInputs(0, 77520);
-                nn.setDesiredOutput(0, 25.56);
-                nn.learn();
-
-                nn.setInputs(0, 83570);
-                nn.setDesiredOutput(0, 25.83);
-                nn.learn();
-
-                nn.setInputs(0, 84470);
-                nn.setDesiredOutput(0, 25.63);
-                nn.learn();
-
-                nn.setInputs(0, 88060);
-                nn.setDesiredOutput(0, 25.93);
-                nn.learn();
-
-                nn.setInputs(0, 97800);
-                nn.setDesiredOutput(0, 25.94);
-                nn.learn();
-
-                nn.setInputs(0, 104410);
-                nn.setDesiredOutput(0, 25.78);
-                nn.learn();
-
-                nn.setInputs(0, 113670);
-                nn.setDesiredOutput(0, 26.02);
-                nn.learn();
-
-                nn.setInputs(0, 124920);
-                nn.setDesiredOutput(0, 26.36);
-                nn.learn();
-
-                nn.setInputs(0, 136590);
+                nn.setInputs(0, 44020);
                 nn.setDesiredOutput(0, 25.91);
                 nn.learn();
 
-                nn.setInputs(0, 142240);
-                nn.setDesiredOutput(0, 26.11);
+                nn.setInputs(0, 47800);
+                nn.setDesiredOutput(0, 26.02);
                 nn.learn();
+
+                nn.setInputs(0, 51510);
+                nn.setDesiredOutput(0, 26.14);
+                nn.learn();
+
+                nn.setInputs(0, 61750);
+                nn.setDesiredOutput(0, 26.01);
+                nn.learn();
+
+                nn.setInputs(0, 67220);
+                nn.setDesiredOutput(0, 25.88);
+                nn.learn();
+
+                nn.setInputs(0, 74790);
+                nn.setDesiredOutput(0, 25.97);
+                nn.learn();
+
+                nn.setInputs(0, 74220);
+                nn.setDesiredOutput(0, 26.65);
+                nn.learn();
+
+                nn.setInputs(0, 71580);
+                nn.setDesiredOutput(0, 26);
+                nn.learn();
+
+                nn.setInputs(0, 72100);
+                nn.setDesiredOutput(0, 26.13);
+                nn.learn();
+
+                nn.setInputs(0, 70480);
+                nn.setDesiredOutput(0, 26.19);
+                nn.learn();
+
+                nn.setInputs(0, 71570);
+                nn.setDesiredOutput(0, 26.07);
+                nn.learn();
+
+                nn.setInputs(0, 73630);
+                nn.setDesiredOutput(0, 25.95);
+                nn.learn();
+
+                nn.setInputs(0, 75140);
+                nn.setDesiredOutput(0, 26.04);
+                nn.learn();
+
+                nn.setInputs(0, 76670);
+                nn.setDesiredOutput(0, 26.14);
+                nn.learn();
+
+                nn.setInputs(0, 69470);
+                nn.setDesiredOutput(0, 26.2);
+                nn.learn();
+
+                nn.setInputs(0, 73220);
+                nn.setDesiredOutput(0, 26.19);
+                nn.learn();
+
+                nn.setInputs(0, 75150);
+                nn.setDesiredOutput(0, 25.87);
+                nn.learn();
+
+                nn.setInputs(0, 76300);
+                nn.setDesiredOutput(0, 25.9);
+                nn.learn();
+
+                nn.setInputs(0, 81930);
+                nn.setDesiredOutput(0, 26.17);
+                nn.learn();
+
+                nn.setInputs(0, 82629);
+                nn.setDesiredOutput(0, 25.93);
+                nn.learn();
+
+                nn.setInputs(0, 86180);
+                nn.setDesiredOutput(0, 26.27);
+                nn.learn();
+
+                nn.setInputs(0, 95500);
+                nn.setDesiredOutput(0, 26.28);
+                nn.learn();
+
+                nn.setInputs(0, 101820);
+                nn.setDesiredOutput(0, 26.15);
+                nn.learn();
+
+                nn.setInputs(0, 111010);
+                nn.setDesiredOutput(0, 26.36);
+                nn.learn();
+
+                nn.setInputs(0, 121960);
+                nn.setDesiredOutput(0, 26.7);
+                nn.learn();
+
+                nn.setInputs(0, 133500);
+                nn.setDesiredOutput(0, 26.26);
+                nn.learn();
+
+                nn.setInputs(0, 138570);
+                nn.setDesiredOutput(0, 26.45);
+                nn.learn();
+
             }
         }
 
